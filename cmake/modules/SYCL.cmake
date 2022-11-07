022ae376cf4b8279a627ddaf061d69ce23cdbb05
wangziyang <wangziyang@ict.ac.cn>
2022-11-07 09:31:37 +0000

    add findSYCL.cmake config

--- cmake/modules/SYCL.cmake
+++ cmake/modules/SYCL.cmake
@@ -15,13 +15,27 @@
 # specific language governing permissions and limitations
 # under the License.
 
+
 if(USE_SYCL)
   message(STATUS "Build with SYCL support")
+  message(STATUS "FOUND CMAKE_PREFIX_PATH : " ${CMAKE_PREFIX_PATH})
+  message(STATUS "FOUND CMAKE_FRAMEWORK_PATH : " ${CMAKE_FRAMEWORK_PATH})
+  message(STATUS "FOUND CMAKE_APPBUNDLE_PATH : " ${CMAKE_APPBUNDLE_PATH})
+  message(STATUS "FOUND PATH : " ${PATH})
   tvm_file_glob(GLOB RUNTIME_SYCL_SRCS src/runtime/sycl/*.cc)
   list(APPEND SYCL_RUNTIME_SRCS ${RUNTIME_SYCL_SRCS})
-
-  include_directories(BEFORE SYSTEM $ENV{SYCL_ROOT_DIR}/include/sycl/)
-  list(APPEND TVM_RUNTIME_LINKER_LIBS $ENV{SYCL_ROOT_DIR}/lib/)
+  if($ENV{SYCL_ROOT_DIR})
+    include_directories(BEFORE SYSTEM $ENV{SYCL_ROOT_DIR}/include/sycl/)
+    list(APPEND TVM_RUNTIME_LINKER_LIBS $ENV{SYCL_ROOT_DIR}/lib/)
+  else()
+    # set(SYCL_DIR "/home/wzy/sycl_workspace/build-cuda")
+    set(SYCL_DIR "/home/wzy/sycl_workspace/tvm-sycl-build-cuda")
+    message(STATUS "FOUND SYCL_DIR : " ${SYCL_DIR})
+    include_directories(BEFORE SYSTEM ${SYCL_DIR}/include/sycl/)
+    list(APPEND TVM_RUNTIME_LINKER_LIBS ${SYCL_DIR}/lib/)
+  endif()
+  # include_directories(BEFORE SYSTEM $ENV{SYCL_ROOT_DIR}/include/sycl/)
+  # list(APPEND TVM_RUNTIME_LINKER_LIBS $ENV{SYCL_ROOT_DIR}/lib/)
 else()
   list(APPEND COMPILER_SRCS src/target/opt/build_sycl_off.cc)
 endif(USE_SYCL)
